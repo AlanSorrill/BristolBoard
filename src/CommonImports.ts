6b2e4b541ce4737a595f64a01fdb06fa2ab083b0
AlanSorrill <alan.sorrill@gmail.com>
2021-10-12 08:30:20 -0500

    Updated Packages

--- src/CommonImports.ts
+++ src/CommonImports.ts
@@ -2,5 +2,5 @@ export * from './Helper'
 export * from './SortedLinkedList'
 export * from './Logger'
 export * from './Animator'
-import * as urlParse from 'url-parse'
+import urlParse from 'url-parse'
 export { urlParse }
